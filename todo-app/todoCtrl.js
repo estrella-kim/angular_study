7ffe68ee54d6d710e14159c0b13523f221da70b6
estrella-kim <puris9030@gmail.com>
2018-01-12 19:23:12 +0900

    todo app 수정

--- todo-app/todoCtrl.js
+++ todo-app/todoCtrl.js
@@ -2,33 +2,32 @@ angular.module('todo', [])
 .controller('todoCtrl', function todoCtrl($scope){
     //모든 목록
     $scope.checkLists = {
-        all : [],
-        active : [],
-        done : [ ]
+        all : ['go'],
+        active : ['string', 'test'],
+        done : ['done']
     };
 
-
     $scope.newList = '';
-    $scope.listsTitle = '';
-    $scope.lists = $scope.checkLists['all'];
+    $scope.listsTitle = 'all';
+    localStorage.setItem('title', '');
+
 
     //필터링
     $scope.filterLists = function (standard) {
-        $scope.lists = $scope.checkLists[standard];
         $scope.listsTitle = standard;
-
     }
+    console.log(localStorage);
 
     //등록
     $scope.createList = function () {
+        localStorage.title = $scope.newList;
         $scope.checkLists.all.push($scope.newList);
         $scope.checkLists.active.push($scope.newList);
         $scope.newList = '';
     }
-
     //카드 이동
     $scope.moveCard = function (index) {
-        var list = $scope.lists[index];
+       /* var list = $scope.lists[index];
         if($scope.listsTitle == 'active') {
             $scope.checkLists.done.push(list);
             $scope.lists.splice(index, 1);
@@ -36,14 +35,14 @@ angular.module('todo', [])
         if($scope.listsTitle == 'done'){
             $scope.checkLists.active.push(list);
             $scope.lists.splice(index, 1);
-        }
+        }*/
 
     }
 
 
     //삭제
     $scope.deleteList = function(index) {
-        $scope.lists.splice(index, 1);
+        // $scope.lists.splice(index, 1);
     }
 
 
